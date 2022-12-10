9329c3cfe8f23b7de2adebf655d471420570cc8e
Jesse Groves <grovesjesse@gmail.com>
2022-12-09 22:07:38 -0500

    added toggling to ribbons

--- js/script.js
+++ js/script.js
@@ -1,20 +1,25 @@
-let ribbons = document.getElementsByClassName('ribbons')[0].getElementsByTagName('*')
+let ribbons = document.getElementsByClassName('ribbon')
 for (let i = 0; i < ribbons.length; i++) {
     ribbons[i].onclick = function () { return clicked(this) }
     ribbons[i].onmouseenter = function () { return grow(this) }
     ribbons[i].onmouseleave = function () { return shrink(this) }
+    ribbons[i].setAttribute("toggled", "false")
 }
 function grow(el) {
     el.style.paddingTop = 20 + "px"
 }
 function shrink(el) {
-    el.style.paddingTop = 10 + "px"
+    if (el.getAttribute("toggled") == "false") {
+        el.style.paddingTop = 10 + "px"
+    }
 }
 function clicked(el) {
     el.style.paddingTop = 20 + "px"
-    let els = el.parentElement.getElementsByClassName('*')
+    el.setAttribute("toggled", "true")
+    let els = el.parentElement.getElementsByTagName('*')
     for (let i = 0; i < els.length; i++) {
-        if (els[i] == el) continue
+        if (els[i] == el) { continue }
+        els[i].setAttribute("toggled", "false")
         shrink(els[i])
     }
 }

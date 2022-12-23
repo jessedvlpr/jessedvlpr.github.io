121ca285c2dbae8af4c9d066ed01864b635bdfad
Jesse Groves <grovesjesse@gmail.com>
2022-12-23 15:00:51 -0500

    removed some code

--- js/project.js
+++ js/project.js
@@ -1,17 +1,6 @@
-function project(thumbnailPath, title, description) {
-    this.thumbnailPath = thumbnailPath
-    this.setThumbnail(this.thumbnailPath)
+function project(thumbnail, title, description, element) {
+    this.thumbnail = thumbnail
     this.title = title
     this.description = description
-}
-
-project.prototype.setThumbnail = function (path) {
-    this.thumbnailPath = path
-    this.element.style.backgroundImage = `url(${this.thumbnailPath})`
-}
-project.prototype.setTitle = function (title) {
-    this.title = title
-}
-project.prototype.setDescription = function (description) {
-    this.description
+    this.element = element
 }

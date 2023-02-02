2350476a28f93a3275548eb3fe69e9b89d3b7084
Jesse Groves <grovesjesse@gmail.com>
2023-02-01 19:38:01 -0500

    added GlyphCrafter to projects

--- Portfolio/js/data.js
+++ Portfolio/js/data.js
@@ -22,8 +22,23 @@ data = `
         ],
         "description": "You're lookin' at it! This is my portfolio website built on plain Vanilla JS/HTML/CSS, displaying all of my previously completed projects. In the future I would like to host this page on a more dynamic platform so it can update automatically when I publish a new project. As of right now, it's hosted on github pages, which only offers static webpage hosting, meaning no databases to hold my projects.",
         "thumbnail": "../portfolio/resources/Portfolio.png",
-        "weblink": "https://jessegroves123.github.io/portfolio/",
-        "repolink": "https://github.com/jessegroves123/jessegroves123.github.io"
+        "weblink": "https://jessegroves123.github.io/Portfolio/",
+        "repolink": "https://github.com/jessegroves123/Portfolio"
+    },
+    "3": {
+        "title": "GlyphCrafter",
+        "delims": [
+            "javascript",
+            "vanilla",
+            "website",
+            "game",
+            "drawing",
+            "magic"
+        ],
+        "description": "A magical Glyph drawing game/tool using SVG to accept user-drawn, symmetrical, circular, smooth shaped 'glyphs', which will then be combined with other glyphs in the same drawing to produce a chemical-reaction-like effect, but magical!"
+        "thumbnail": "../portfolio/resources/GlyphCrafter.png",
+        "weblink": "https://jessegroves123.github.io/GlyphCrafter/",
+        "repolink": "https://github.com/jessegroves123/GlyphCrafter"
     }
 }
 `
